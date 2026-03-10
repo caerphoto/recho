bcbc8e9c66ca978fdb871f5b41f2e0c66f22f037
Andy Farrell <github@andyf.me>
2026-03-10 11:21:19 +0000

    Reformat echo fn params for readability

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,13 @@ use axum::{
 };
 use colored::Colorize;
 
-async fn echo(uri: OriginalUri, method: Method, Query(params): Query<HashMap<String, String>>, headers: HeaderMap, body: String) {
+async fn echo(
+    uri: OriginalUri,
+    method: Method,
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+    body: String)
+{
     if body.is_empty() {
         log::info!("Request to: {}\nQuery:\n{}\nHeaders:\n{}\n(no request body)",
             format!("{} {}", method, uri.path()).magenta(),
@@ -25,9 +31,7 @@ async fn echo(uri: OriginalUri, method: Method, Query(params): Query<HashMap<Str
 async fn main() {
     env_logger::init();
 
-    let app = Router::new()
-        // .route("/", any(echo))
-        .fallback(echo);
+    let app = Router::new().fallback(echo);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4000").await.unwrap();
 
